6ab996a059c7ea2a2af998ce04f50595ad392e53
Camilo Caetano <camilo.s.caetano@gmail.com>
2023-04-16 14:44:58 -0300

    add exemple python create a simple api

--- python/redisample/README.md
+++ python/redisample/README.md
@@ -9,3 +9,8 @@ docker run --name my-redis --network redis-net -d redis
 
 docker exec -it my-redis redis-cli
 
+#Execute docker compose
+
+This  for building a docker container each time you run a command
+
+ docker-compose up --build 
--- python/redisample/app.py
+++ python/redisample/app.py
@@ -1,21 +1,40 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import redis
 
 app = Flask(__name__)
 
 # Crie uma conexão com o Redis
+
 r = redis.Redis(host='redis', port=6379, db=0)
+
 
 @app.route('/')
 def hello():
-    # Defina uma chave e valor no Redis
-    r.set('mykey', 'Hello, Redis!')
-
+     return jsonify({'message': 'Hello, Python'})
+@app.route('/getredis')
+def getredis():
     # Obtenha o valor da chave no Redis
     value = r.get('mykey')
 
-    # Retorne o valor como uma resposta HTTP
-    return value.decode('utf-8')
+    # Verifique se o valor é None antes de tentar decodificá-lo
+    if value == None:
+        return "Chave 'mykey' não encontrada no Redis"
+    else:
+        # Retorne o valor como uma resposta HTTP
+        return value.decode('utf-8')
+
+
+@app.route('/add')
+def add():
+
+    name = request.args.get('name')
+    
+    if name is not None:
+        r.set('mykey', name)
+    else:
+        return jsonify({'message': 'Não foi possível adicionar o valor'}) 
+
+    return jsonify({'message': 'Valor salvo com sucesso'}) 
 
 @app.route('/helloworld')
 def hello_world():
